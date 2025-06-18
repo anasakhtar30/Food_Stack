090a7bc192e9e4b56319d910d6036d67bc6c72d2
AnkitaKoley81 <ankitakoley81@gmail.com>
2025-06-18 18:21:56 +0530

    Updated Navbar and assets

--- admin1/src/assets/admin_assets/assets.js
+++ admin1/src/assets/admin_assets/assets.js
@@ -2,15 +2,19 @@ import logo from './logo.png'
 import add_icon from './add_icon.png'
 import order_icon from './order_icon.png'
 import profile_image from './profile_image.png'
+import profile_me from './profile_me.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 import meri from './meri.png'
+import mai from './mai.png'
 
 export const assets ={
     logo,
+    mai,
     add_icon,
     order_icon,
     profile_image,
+    profile_me,
     upload_area,
     parcel_icon,
     meri
